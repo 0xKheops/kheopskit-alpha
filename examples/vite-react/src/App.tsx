d64e5971826dbdd025b63e41f60b8d8aef1ed59b
0xKheops <26880866+0xKheops@users.noreply.github.com>
2025-05-04 19:44:27 +0900

    init

---
+++ examples/vite-react/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="connect-buttons">
+        <button>Connect Polkadot</button>
+        <button>Connect Ethereum</button>
+        <button>Connect Polkadot or Ethereum</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export const connect = () => {
+  console.log("connect");
+};
